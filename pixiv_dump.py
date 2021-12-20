5c6f4d010e2b000a253395d9a46effd1c4776ebf
Elrori <zhouchaoyu2013@icloud.com>
2021-12-20 12:09:34 +0800

    modify

--- pixiv_dump.py
+++ pixiv_dump.py
@@ -3,13 +3,15 @@ import sys
 import requests
 from lxml import etree
 import re
-from fake_useragent import UserAgent
-proxy   = {"http": "socks5://127.0.0.1:1082","https": "socks5://127.0.0.1:1082"}
+# from fake_useragent import UserAgent
 
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-#            'accept-language': 'zh-TW;q=0.9,en;q=0.8,ja;q=0.7,zh-CN,zh;q=0.6', }
-headers = {'User-Agent': UserAgent(verify_ssl=False).random,
+# Set your proxy first
+proxy   = {"http": "socks5://192.168.1.1:1082","https": "socks5://192.168.1.1:1082"}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
            'accept-language': 'zh-TW;q=0.9,en;q=0.8,ja;q=0.7,zh-CN,zh;q=0.6', }
+# headers = {'User-Agent': UserAgent(verify_ssl=False).random,
+#            'accept-language': 'zh-TW;q=0.9,en;q=0.8,ja;q=0.7,zh-CN,zh;q=0.6', }
 
 main_url= 'https://www.pixiv.net/'
 
@@ -76,11 +78,17 @@ def dump_rank_img(session=None,mode='daily',page_num=1,page_sz=50):
     rank=1
     for art_url in art_urls:
         print('Get '+art_url,end='')
-        imag_name,raw_bin = get_image_raw(session,art_url)
-        path = saved_dir+'/'+str(rank)+'_'+imag_name
-        with open(path,'wb') as f:
-            f.write(raw_bin)
-            print('=> '+path)
+        try:
+            imag_name,raw_bin = get_image_raw(session,art_url)
+        except KeyboardInterrupt :
+            exit(0)
+        except :
+            print(f" error {rank}",end='\n')
+        else:
+            path = saved_dir+'/'+str(rank)+'_'+imag_name
+            with open(path,'wb') as f:
+                f.write(raw_bin)
+                print('=> '+path)
         rank+=1
     
 
@@ -90,4 +98,5 @@ if __name__ == '__main__':
     # dump_rank_img(session,'daily',1,50)
     # dump_rank_img(session,'weekly',1,50)
     dump_rank_img(session,'monthly',2,50)
+
 
